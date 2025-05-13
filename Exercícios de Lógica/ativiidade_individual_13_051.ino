f92334d27cab0b2b36c7da70680194400285c15f
Gustavo Oliveira <henriquedossantosgustavo61@gmail.com>
2025-05-13 16:46:47 -0300

    foi adicionado uma arquivo chamdado atividade individual

---
+++ Exercícios de Lógica/ativiidade_individual_13_051.ino
@@ -0,0 +1,31 @@
+// C++ code
+//
+ 
+ //variáveis
+
+int resultado;
+int tabuadaEscolhida;
+
+void setup()
+{
+ Serial.begin(9600); 
+}
+
+void loop()
+{ 
+  Serial.println("Bem-vindo(a)! :");
+  Serial.println("Digite qual tabuada você deseja ver :");
+  while(!Serial.available ()) {} //Espera o usuário
+  tabuadaEscolhida = Serial.parseInt();
+  
+  
+  for (int contador = 1; contador <= 10; contador++ ){
+  resultado = tabuadaEscolhida * contador;
+    
+   // Exibir assim: 2 x 1 =20
+   Serial.println(String(tabuadaEscolhida) + " X " + String(contador) + " = " + String(resultado)); 
+  
+  }
+  
+  
+}  
